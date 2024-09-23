50fdf19c22942af90080efbced42c1710315ebf1
LorensiusGani <lorensiusgani08@gmail.com>
2024-09-23 21:53:52 +0700

    Update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const Contact = () => {
+
   return (
     <div className="h-screen w-full bg-black pt-20">
       <div className='text-white'>
@@ -12,20 +13,20 @@ const Contact = () => {
       </div>
 
       <div className='flex justify-center items-center mt-10'>
-        <form action="" className='flex flex-col w-full md:w-1/2'>
-          <input type="text" name="" id="" 
+        <form className='form flex flex-col w-full md:w-1/2'>
+          <input type="text" name="nama" id="" 
           placeholder='Enter your Name'
           className='p-2 bg-transparent border-4 text-white mb-2'
           />
-          <input type="text" name="" id="" 
+          <input type="text" name="email" id="" 
           placeholder='Enter your Email'
           className=' p-2 bg-transparent border-4 text-white my-2'
           />
-          <input type="text" name="" id="" 
-          placeholder='Enter your number phone'
-          className=' p-2 bg-transparent border-4 text-white my-2'
+          <input type="text" name="pesan" id="" 
+          placeholder='Enter your message'
+          className=' p-6 bg-transparent border-4 text-white my-2'
           />
-          <button className='bg-blue-500 text-black font-bold px-6 py-3 my-6 mx-auto rounded-xl cursor-pointer hover:scale-90 hover:bg-white hover:text-black duration-200'>Submit</button>
+          <button type='submit' className='bg-blue-500 text-black font-bold px-6 py-3 my-6 mx-auto rounded-xl cursor-pointer hover:scale-90 hover:bg-white hover:text-black duration-200'>Submit</button>
         </form>
       </div>
 
